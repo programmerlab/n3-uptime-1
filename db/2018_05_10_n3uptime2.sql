7b2b14b8dba4814e6a62d3732ce4fdb67ec74071
koushikgraj <37832167+koushikgraj@users.noreply.github.com>
2018-05-16 21:12:12 +0530

    N3-3133 | Fix the wrong column name in database schema (#5)

--- db/2018_05_10_n3uptime2.sql
+++ db/2018_05_10_n3uptime2.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS `domains` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `doamin` varchar(253) NOT NULL,
+  `domain` varchar(255) NOT NULL,
   `expected_status` int(3) DEFAULT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '0',
   `created` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY (`doamin`)
+  UNIQUE KEY (`domain`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20938 DEFAULT CHARSET=utf8;
 
 
